76b15fe0fac74936306d8fa5dd6529d185893fad
Amit <lkyadav090@gmail.com>
2024-04-04 22:01:33 +0530

    Homepage Product get routes seprated

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 import express from "express";
 import "./db.js";
-import {
-  Cargos,
-  Sweatpants,
-  Hoodies,
-  Tshirts,
-  Shirts,
-} from "./Database/products.js";
 import bodyParser from "body-parser";
 import cors from "cors";
 import fileUpload from "express-fileupload";
 import "./plugins/cloudinary.js";
 import dotenv from "dotenv";
 import adminRoute from "./routes/admin.js";
+import productRouter from "./routes/product.js";
 dotenv.config();
 
 const app = express();
@@ -21,18 +15,7 @@ app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 
-app.get("/get/sweatpants", async (req, res) => {
-  const data = await Sweatpants.find();
-
-  res.send(data);
-});
-
-app.get("/get/cargos", async (req, res) => {
-  const data = await Cargos.find();
-
-  res.send(data);
-});
-
+app.use("/get", productRouter);
 app.use("/admin", adminRoute);
 
 app.listen(3000, () => console.log("server is running on port 3000"));
---
+++ routes/product.js
@@ -0,0 +1,34 @@
+import express from "express";
+import {
+  Cargos,
+  Sweatpants,
+  Hoodies,
+  Tshirts,
+  Shirts,
+} from "../Database/products.js";
+
+const productRouter = express.Router();
+
+productRouter.get("/sweatpants", async (req, res) => {
+  const data = await Sweatpants.find();
+  res.send(data);
+});
+
+productRouter.get("/cargos", async (req, res) => {
+  const data = await Cargos.find();
+  res.send(data);
+});
+
+productRouter.get("/tshirts", async (req, res) => {
+  const data = await Tshirts.find();
+  res.send(data);
+});
+productRouter.get("/shirts", async (req, res) => {
+  const data = await Shirts.find();
+  res.send(data);
+});
+productRouter.get("/hoodies", async (req, res) => {
+  const data = await Hoodies.find();
+  res.send(data);
+});
+export default productRouter;
