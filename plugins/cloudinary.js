899b7dbfe7e15ffba0903025ad34817eac048fa0
Amit <lkyadav090@gmail.com>
2024-05-17 01:03:43 +0530

    cloudinary security added

--- plugins/cloudinary.js
+++ plugins/cloudinary.js
@@ -1,6 +1,7 @@
-import {v2 as cloudinary} from "cloudinary"
+import { v2 as cloudinary } from "cloudinary";
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME_CLOUDINARY,
-    api_key: process.env.API_KEY_CLOUDINARY,
-    api_secret: process.env.API_SECRET_CLOUDINARY,
-  });
+  cloud_name: process.env.CLOUD_NAME_CLOUDINARY,
+  api_key: process.env.API_KEY_CLOUDINARY,
+  api_secret: process.env.API_SECRET_CLOUDINARY,
+  secure: true,
+});
