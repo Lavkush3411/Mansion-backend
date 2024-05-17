02ffe53d5e4d14b3373960ecee4275f2edfc189c
Amit <lkyadav090@gmail.com>
2024-05-17 11:23:12 +0530

    added cors policy

--- index.js
+++ index.js
@@ -16,10 +16,20 @@ const app = express();
 app.use(fileUpload({ useTempFiles: true }));
 app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+
+const corsOptions = {
+  origin: "mansionstreetwear.netlify.app", // Replace with your client URL
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // If you need to send cookies or authentication headers
+  optionsSuccessStatus: 204, // Some legacy browsers choke on 204
+};
+
+app.use(cors(corsOptions));
+
 app.use(cors());
 
 app.use("/get", productRouter);
-app.use("/admin", verifyToken, verifyAdmin,adminRoute);
+app.use("/admin", verifyToken, verifyAdmin, adminRoute);
 app.use("/user", userRoute);
 
 app.listen(3000, () => console.log("server is running on port 3000"));
