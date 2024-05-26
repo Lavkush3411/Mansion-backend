40d04c659c805ec5c571a4d157c1e826011852a3
Amit <lkyadav090@gmail.com>
2024-05-26 11:02:01 +0530

    added lean method on data fetching from mongodb

--- routes/product.js
+++ routes/product.js
@@ -11,31 +11,37 @@ const productRouter = express.Router();
 
 const productList = [Cargos, Sweatpants, Hoodies, Tshirts, Shirts];
 
+let alldata;
+
+// setInterval(() => {}, interval);
+
 productRouter.get("/all", async (req, res) => {
-  const alldata = await Promise.all(productList.map((item) => item.find()));
+  const alldata = await Promise.all(
+    productList.map((item) => item.find().lean())
+  );
   res.send(alldata.flat());
 });
 
 productRouter.get("/sweatpants", async (req, res) => {
-  const data = await Sweatpants.find();
+  const data = await Sweatpants.find().lean();
   res.send(data);
 });
 
 productRouter.get("/cargos", async (req, res) => {
-  const data = await Cargos.find();
+  const data = await Cargos.find().lean();
   res.send(data);
 });
 
 productRouter.get("/tshirts", async (req, res) => {
-  const data = await Tshirts.find();
+  const data = await Tshirts.find().lean();
   res.send(data);
 });
 productRouter.get("/shirts", async (req, res) => {
-  const data = await Shirts.find();
+  const data = await Shirts.find().lean();
   res.send(data);
 });
 productRouter.get("/hoodies", async (req, res) => {
-  const data = await Hoodies.find();
+  const data = await Hoodies.find().lean();
   res.send(data);
 });
 export default productRouter;
