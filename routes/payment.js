629da5906be3af371e389b80f900c7f121700632
Amit <lkyadav090@gmail.com>
2024-08-03 21:53:28 +0530

    Payment Status check will be perfromed when webhook is called

--- routes/payment.js
+++ routes/payment.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
   initiatePayment,
-  paymentCallback,
   paymentStatus,
 } from "../controllers/paymentController.js";
 import createOrder from "../middlewares/createOrder.middleware.js";
@@ -9,6 +8,5 @@ const paymentRouter = express.Router();
 
 paymentRouter.route("/initiate").post(createOrder, initiatePayment);
 paymentRouter.route("/status/:transactionID").post(paymentStatus);
-paymentRouter.route("/callback/:transactionID").post(paymentCallback);
 
 export default paymentRouter;
